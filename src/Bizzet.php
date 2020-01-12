1a2e15b46f3f916944e4b52f184047408dfb27a1
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2020-01-13 03:24:51 +0700

    editorconfig

--- src/Bizzet.php
+++ src/Bizzet.php
@@ -1,15 +1,12 @@
 <?php
+/**
+ * File: Bizzet.php
+ * @author: Cvar1984 <gedzsarjuncomuniti@gmail.com>
+ * Date: 13.01.2020
+ * Last Modified Date: 13.01.2020
+ * Last Modified By: Cvar1984 <gedzsarjuncomuniti@gmail.com>
+ */
 namespace Cvar1984\Bizzet;
-
-interface BizzetInterface
-{
-    public function __construct();
-    public function __destruct();
-    public function getUserAgent();
-    public function getReferer();
-    public function getProxy(array $options);
-    public function request(string $url, array $options);
-}
 
 class Bizzet implements BizzetInterface
 {
